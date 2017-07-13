108309bb89ff569ea178c056c5d43072e1ca92d5
wm283 <wm283@cornell.edu>
2017-07-13 13:47:21 -0400

    Add files via upload

---
+++ IllegalCharException.java
@@ -0,0 +1,11 @@
+package a3_1;
+
+public class IllegalCharException extends Exception {
+	public IllegalCharException(String m) {
+		super(m);
+	}
+	public IllegalCharException() {
+		super();
+	}
+
+}
